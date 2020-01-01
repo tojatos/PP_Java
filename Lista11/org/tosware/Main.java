3bb120479222e02982a0c3c28b8c652b2e95f80a
Krzysztof <krzysztof.ruczkowski@nokia.com>
2020-01-01 20:32:31 +0100

    Lista 11 - zad1 done

---
+++ Lista11/org/tosware/Main.java
@@ -0,0 +1,40 @@
+package org.tosware;
+
+class IntCell {
+	private int n = 0;
+
+	public int getN() {
+		return n;
+	}
+
+	public void setN(int n) {
+		this.n = n;
+	}
+}
+
+class Count extends Thread {
+	private static IntCell n = new IntCell();
+	@Override
+	public void run() {
+		int temp;
+		for (int i = 0; i < 200000; i++) {
+			temp = n.getN();
+			n.setN(temp + 1);
+			System.out.println("The value of n is " + n.getN());
+			/**
+			 * Variables sometimes can be set based on invalid temp value
+			 */
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Count p = new Count();
+		Count q = new Count();
+		p.start();
+		q.start();
+		try {p.join(); q.join();}
+		catch (InterruptedException e) {}
+	}
+}
